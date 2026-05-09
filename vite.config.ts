293aee1a48711000ad0b3b10f63b236d8457ec56
Richard Kabiling <richardkabiling@gmail.com>
2026-05-09 20:40:01 +0800

    chore: update Node.js version to 22 in GitHub Actions and set FORCE_JAVASCRIPT_ACTIONS_TO_NODE24 environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/odins-workshop/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['glpk.js'],
